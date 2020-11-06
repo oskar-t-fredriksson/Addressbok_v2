cf72ed09481e220812d94fd4c641a824d8f2c8c9
oskar-t-fredriksson <72459215+oskar-t-fredriksson@users.noreply.github.com>
2020-11-06 21:34:02 +0100

    Finished code

--- Program.cs
+++ Program.cs
@@ -10,31 +10,40 @@ namespace Addressbok_v2
 {
     class Program
     {
+        class ContactBook
+        {
+            public string name, address, phone, email;
+            public ContactBook(string N, string A, string P, string E)
+            {
+                name = N; address = A; phone = P; email = E; 
+            }
+            public string Person()
+            {
+                return $"{name}, {address}, {phone}, {email}";
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Välkommen till Kontaktboken!");
-            ContactBookMenu();
-            Console.WriteLine("> ");
-            string command = Console.ReadLine();
             //C:\\Users\\oskar\\adressbok.txt
+            Console.WriteLine("Välkommen till Kontaktboken!");
+            string command;
             do
             {
+                ContactBookMenu();
+                Console.WriteLine("> ");
+                command = Console.ReadLine();
                 if (command == "1")
                 {
-                    Writer();
-                    break;
-
+                    Writer();                    
                 }
                 else if (command == "2")
                 {
                     Remover();
-                    break;
 
                 }
                 else if (command == "3")
                 {
                     Reader();
-                    break;
                 }
             }
             while (command != "4");
@@ -42,32 +51,50 @@ namespace Addressbok_v2
         //https://stackoverflow.com/questions/7569904/easiest-way-to-read-from-and-write-to-files code source
         static void ContactBookMenu()
         {
-            Console.WriteLine("********Menyval********");
-            Console.WriteLine("1. Lägg till kontakt");
-            Console.WriteLine("2. Ta bort kontakt");
-            Console.WriteLine("3. Visa kontakter");
-            Console.WriteLine("4. Avsluta Kontaktboken");
-            Console.WriteLine("***********************");
+            Console.WriteLine("**********Menyval**********");
+            Console.WriteLine("* 1. Lägg till kontakt    *");
+            Console.WriteLine("* 2. Ta bort kontakt      *");
+            Console.WriteLine("* 3. Visa kontakter       *");
+            Console.WriteLine("* 4. Avsluta Kontaktboken *");
+            Console.WriteLine("***************************");
         }
         static void Writer()
         {
             string path = @"C:\\Users\\oskar\\adressbok.txt";
-            Console.WriteLine("Ange kontaktinfo(Namn, Adress, Telefon och Email): ");
-            string addContactToTxt = Console.ReadLine() + Environment.NewLine + "\n";
-            File.AppendAllText(path, addContactToTxt, Encoding.UTF8);
+            Console.WriteLine("Ange kontaktinfo, 'Namn', 'Adress', 'Telefon' och 'Email'");
+            Console.WriteLine("Namn: ");
+            string addNameToTxt = Console.ReadLine() + ",";
+            File.AppendAllText(path, addNameToTxt, Encoding.UTF8);
+            Console.WriteLine("Address: ");
+            string addAdressToTxt = Console.ReadLine() + ",";
+            File.AppendAllText(path, addAdressToTxt, Encoding.UTF8);
+            Console.WriteLine("Telefonnummer: ");
+            string addPhoneToTxt = Console.ReadLine() + ",";
+            File.AppendAllText(path, addPhoneToTxt, Encoding.UTF8);
+            Console.WriteLine("E-post: ");
+            string addEmailToTxt = Console.ReadLine() + "\n";
+            File.AppendAllText(path, addEmailToTxt, Encoding.UTF8);
+            List<ContactBook> book = new List<ContactBook>();
+            book.Add(new ContactBook(addNameToTxt, addAdressToTxt, addPhoneToTxt, addEmailToTxt));
         }
         static void Remover()
         {
             string path = @"C:\\Users\\oskar\\adressbok.txt";
-            string removeTextFromTxt = Console.ReadLine() + Environment.NewLine;
-            File.WriteAllText(path, removeTextFromTxt);
+            List<string> oldContacts = File.ReadAllLines(path).ToList();
+            Console.Write("Vilket nummer vill du ta bort: ");
+            int num = int.Parse(Console.ReadLine());
+            oldContacts.RemoveAt(num);
+            //Found help from https://www.c-sharpcorner.com/blogs/how-to-remove-a-line-from-a-text-file-at-specific-location-in-c-sharp1
+            File.WriteAllLines((path), oldContacts.ToArray());
         }
         static void Reader()
         {
             string path = @"C:\\Users\\oskar\\adressbok.txt";
-            string readContactsFromTxt = File.ReadAllText(path);
-            Console.WriteLine($"{readContactsFromTxt}");
+            List<string> oldContacts = File.ReadAllLines(path).ToList();
+            for (int i = 0; i < oldContacts.Count; i++)
+            {
+                Console.WriteLine($"{i}: {oldContacts[i]}");
+            }
         }
-
     }
 }
